a480de1df7129b86971f1767959fa719fcfce0c8
Bronislav <shevchenko-bg@yandex.ru>
2024-11-13 18:45:49 +0300

    was added lesson 10 - seminar

---
+++ Урок 10. Семинар. Работа с JSON/data.js
@@ -0,0 +1,22 @@
+const datainfo = `
+[
+  {
+    "id": "BQMSld0A0",
+    "url": "https://cdn2.thecatapi.com/images/BQMSld0A0.jpg",
+    "width": 1080,
+    "height": 1080
+  },
+  {
+    "id": "dhs",
+    "url": "https://cdn2.thecatapi.com/images/dhs.jpg",
+    "width": 2719,
+    "height": 2039
+  },
+  {
+    "id": "7dr",
+    "url": "https://cdn2.thecatapi.com/images/7dr.jpg",
+    "width": 500,
+    "height": 358
+  }
+]
+`
