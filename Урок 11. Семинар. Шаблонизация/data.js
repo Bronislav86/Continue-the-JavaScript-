97f1466ca00e5dc7c550d598af1b7af240ea504e
Bronislav <shevchenko-bg@yandex.ru>
2024-11-14 22:20:58 +0300

    was added lesson 11 seminar and HW for this lesson

---
+++ Урок 11. Семинар. Шаблонизация/data.js
@@ -0,0 +1,28 @@
+const dataInfo = `
+[
+  {
+    "name": "MANGO  PEOPLE  T-SHIRT",
+    "url": "./Product_photo_1.png",
+    "price": "$300",
+    "color": "Red",
+    "size": "Xl",
+    "quantity": "2"
+  },
+  {
+    "name": "ELLERY X M'O CAPSULE",
+    "url": "./Product_photo_2.png",
+    "price": "$52",
+    "color": "Black",
+    "size": "Xl",
+    "quantity": "1"
+  },
+  {
+    "name": "ELLERY X M'O CAPSULE",
+    "url": "./Product_photo_1.png",
+    "price": "$522",
+    "color": "Green",
+    "size": "L",
+    "quantity": "1"
+  }
+]
+`
